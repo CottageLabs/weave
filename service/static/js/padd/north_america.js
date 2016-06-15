3c5d39651a051a3f382df0c001f3e3dce5ea5a5f
Richard Jones <richard@cottagelabs.com>
2016-06-15 11:25:13 +0100

    update north america interface

--- service/static/js/padd/north_america.js
+++ service/static/js/padd/north_america.js
@@ -1,18 +1,78 @@
 var na = {
     activeEdge : false,
 
+    COUNTRY_ID_MAP : {
+        "US" : "USA",
+        "Canada" : "CAN"
+    },
+
+    preFilterCSVs : function(params) {
+        var resource = params.resource;
+        var year = (new Date()).getUTCFullYear().toString();
+        resource.add_filter({filter: {field: "From", value: year, type: "exact"}});
+        resource.add_filter({filter: {field: "Area", value: "", type: "exact"}});
+    },
+
+    conProdSync : function(component) {
+        // FIXME: right now this function is just a demo of the sync feature, we need to pay more attention to the
+        // filtering and the values
+        var resources = component.edge.resources;
+        var consumption = resources.consumption;
+        var production = resources.production;
+
+        var coniter = consumption.iterator();
+        var res = coniter.next();
+        while (res) {
+            var val = res["Value"];
+            var id = res["Country"];
+            var from = res["From"];
+            var key = "Consumption (" + from + ")";
+            id = na.COUNTRY_ID_MAP[id];
+            component.regionData[id] = {};
+            component.regionData[id][key] = val;
+            res = coniter.next();
+        }
+
+        var proditer = production.iterator();
+        var res = proditer.next();
+        while (res) {
+            var val = res["Value"];
+            var id = res["Country"];
+            var type = res["Production source"];
+            var from = res["From"];
+            var key = "Production, " + type + " (" + from  + ")";
+            id = na.COUNTRY_ID_MAP[id];
+            component.regionData[id][key] = val;
+            res = proditer.next();
+        }
+    },
+
     create : function() {
 
         var e = edges.newEdge({
             selector: "#world",
             template: na.newTemplate(),
             staticFiles : [
-                {"id" : "consumption", "url" : "/static/data/acuity/consumption.csv", "processor" : edges.csv.newObjectByRow, "datatype" : "text"},
-                {"id" : "production", "url" : "/static/data/acuity/production.csv", "processor" : edges.csv.newObjectByRow, "datatype" : "text"}
+                {id : "consumption", url : "/static/data/acuity/consumption.csv", processor : edges.csv.newObjectByRow, datatype : "text", opening: na.preFilterCSVs},
+                {id : "production", url : "/static/data/acuity/production.csv", processor : edges.csv.newObjectByRow, datatype : "text", opening: na.preFilterCSVs}
             ],
             components : [
+                edges.newStaticDataSelector({
+                    id: "year-selector",
+                    resourceId: "consumption",
+                    filterResourceIds : ["consumption", "production"],
+                    field: "From",
+                    display: "Year",
+                    renderer : edges.bs3.newNSeparateORTermSelectorRenderer({
+                        n: 1,
+                        properties : [
+                            {label: "Year", unselected: "<select a year>"}
+                        ]
+                    })
+                }),
                 edges.newRegionDataMap({
                     id: "na-map",
+                    synchronise: na.conProdSync,
                     renderer : edges.d3.newGenericVectorMap({
                         width : 1100,
                         height : 700,
@@ -53,7 +113,7 @@ var na = {
         this.draw = function(edge) {
             this.edge = edge;
 
-            var frag = '<div class="row">\
+            var frag = '<div class="row"><div class="col-md-offset-5 col-md-2"><div id="year-selector"></div></div></div><div class="row">\
                 <div class="col-md-12">\
                     <div id="' + id + '"></div>\
                 </div>\
